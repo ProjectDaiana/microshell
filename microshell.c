6ea1d1dea92e73892cb55eeab2360d93dfb8abf6
Daiana Rotchen <darotche@c4a14c8.42berlin.de>
2024-08-12 18:08:39 +0200

    execve, cd and print error

--- microshell.c
+++ microshell.c
@@ -2,10 +2,11 @@
 #include <string.h>
 #include <unistd.h>
 
-int errors(char *msg)
+int errors(char *msg, char *str)
 {
 
 	write(2, msg, strlen(msg));
+	write(2, str, strlen(str));
 	write(2, "\n", 1);
 	return 1;
 }
@@ -19,9 +20,11 @@ int main(int argc, char **argv, char **env)
 			printf("microshell: %s\n", argv[i]);
 			while(argv[i])
 			{
+				printf("argv[%d]: %s\n", i, argv[i]);
 				if(!strcmp(argv[i], ";"))
 				{
 					i++;
+					printf("skipping ; i: %s\n", argv[i]);
 					continue;
 				}
 				// else if(strcmp(argv[i], "|"))
@@ -30,7 +33,6 @@ int main(int argc, char **argv, char **env)
 				// }
 				char *cmd[] = {argv[i], NULL}; 
 				printf("cmd: %s\n", cmd[0]);
-
 				//EXECUTE cd or path
 				//Search for "/" in the command line
 				// If found, execute the command
@@ -38,8 +40,12 @@ int main(int argc, char **argv, char **env)
 				if (strcmp(argv[i], "cd") == 0)
 				{
 					if(chdir(argv[i + 1]) == -1)
-						errors("Error: chdir");
-					printf("Directory changed to %s\n", argv[i + 1]);										
+						errors("error: cd: cannot change directory to ", argv[i + 1]);
+					char cwd[1024];
+                    if (getcwd(cwd, sizeof(cwd)) != NULL)
+                    {
+                        printf("Directory changed to: %s\n", cwd);
+                    }									
 					// int e = 0;
 					// while (env[e])
 					// {
@@ -47,8 +53,9 @@ int main(int argc, char **argv, char **env)
 					// 	e++;
 					// }
 				}
+				printf("executing: %s\n", argv[i]);
 				if(execve(argv[i], cmd, env) == -1)
-					errors("error: cannot execute executable_that_failed");
+					errors("error: cannot execute ", argv[i]);
 				i++;
 			}
 	}

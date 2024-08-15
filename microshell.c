eeafb36ac904fa6e2538a59fa972a9f9592b356e
Daiana Rotchen <darotche@c4a14c7.42berlin.de>
2024-08-15 18:20:43 +0200

    improve in arch

--- microshell.c
+++ microshell.c
@@ -9,23 +9,52 @@ int errors(char *msg)
 	return 1;
 }
 
-int	exec_cd(char *arg, int i)
+void print_cwd()
 {
-	if (arg == NULL || i < 2)
-		errors("error: cd: bad arguments");
-	if(chdir(arg) == -1)
-	{
-		errors("error: cd: cannot change directory to ");
-		errors(arg);
-		errors("\n");
-	}
-	//NEXT 5 LINES ONLY FOR DEBUGGING
 	char cwd[1024];
 	if (getcwd(cwd, sizeof(cwd)) != NULL)
 	{
 		printf("Current Directory: %s\n", cwd);
 	}
+}
+
+int	exec_cd(char *arg, int i)
+{
+	print_cwd();
+	if (arg == NULL || i < 2)
+	{
+		errors("error: cd: bad arguments");
+		return(1);
+	}
+	if(chdir(arg) == -1)
+	{
+		errors("error: cd: cannot change directory to ");
+		errors(arg);
+		errors("\n");
+		return(1);
+	}
+	print_cwd();
 	return(0);	
+}
+
+int	exec_cmd(char **cmd, int i, char **env)
+{
+	int i;
+	i = 0;
+	if (strcmp(cmd, "cd") == 0)
+	{
+		exec_cd(argv[i + 1], i);
+		return (0);
+	}
+	else {
+		printf("EXECUTING: %s\n", argv[i]);
+		if(execve(argv[i], cmd, env) == -1)
+		{
+			errors("error: cannot execute ");
+			errors(argv[i]);
+			errors("\n");
+		}
+	}
 }
 
 int main(int argc, char **argv, char **env)
@@ -34,45 +63,25 @@ int main(int argc, char **argv, char **env)
 
 	if (argc > 1)
 	{
-			while(argv[i])
+		while(argv[i])
+		{
+			printf("argv[%d]: %s\n", i, argv[i]);
+			if (!strcmp(argv[i], ";"))
 			{
-				int pid = fork();
-
-				if (pid == -1)
-					errors("error: cannot fork\n");
-				if (pid == 0)
-				{
-					printf("argv[%d]: %s\n", i, argv[i]);
-					if(!strcmp(argv[i], ";"))
-					{
-						i++;
-						printf("skipping ; i: %s\n", argv[i]);
-						continue;
-					}
-					// else if(strcmp(argv[i], "|"))
-					// {
-					// 	dup2(argv[i]);
-					// }
-					char *cmd[] = {argv[i], NULL}; 
-					printf("cmd: %s\n", cmd[0]);
-					//EXECUTE cd or path
-					//Search for "/" in the command line If found, execute the command
-					// else check if is cd and has an argument
-					if (strcmp(argv[i], "cd") == 0)
-						exec_cd(argv[i + 1], i);	
-					printf("EXECUTING: %s\n", argv[i]);
-					if(execve(argv[i], cmd, env) == -1)
-					{
-						errors("error: cannot execute ");
-						errors(argv[i]);
-						errors("\n");
-					}
-					else
-						waitpid(pid, NULL, 0);
-				}
 				i++;
-				printf("Continues w next command: %d\n", pid);
+				continue;
 			}
+			int pid = fork();
+			if (pid == -1)
+				errors("error: cannot fork\n");
+			if (pid == 0)
+			{
+				exec_cmd(argv[i], i, env);
+			}
+			waitpid(pid, NULL, 0);
+			i++;
+			printf("Continues w next command: %s\n", argv[i]);
+		}
 	}
 	return (0);
 }

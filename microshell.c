5d36eaad178eeae8154264f31c3e36b5391cdd13
ProjectDaiana <110055045+ProjectDaiana@users.noreply.github.com>
2024-08-21 10:21:17 +0200

    Update microshell.c, grademe SUCCESS

--- microshell.c
+++ microshell.c
@@ -1,108 +1,73 @@
-#include <stdio.h>
 #include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
 #include <stdlib.h>
+#include <stdio.h>
 
-int errors(char *msg)
+void err(char *str)
 {
-	while (*msg)
-		write(2, msg++, 1);
-	return 1;
+        while (*str)
+                write(2, str++, 1); 
 }
 
-void print_cwd()
+int cd(char **argv, int i)
 {
-	char cwd[1024];
-	if (getcwd(cwd, sizeof(cwd)) != NULL)
-	{
-		printf("Current Directory: %s\n", cwd);
-	}
-}
-
-int	exec_cd(char *arg, int i)
-{
-	//print_cwd();
-	if (arg == NULL || i != 2)
-	{
-		errors("error: cd: bad arguments");
-		errors("\n");
-		exit(1);
-	}
-	if(chdir(arg) == -1)
-	{
-		errors("error: cd: cannot change directory to ");
-		errors(arg);
-		errors("\n");
-		exit(1);
-	}
-	//print_cwd();
-	return(0);	
+        if (i != 2)
+                return err("error: cd: bad arguments\n"), 1;
+        if (chdir(argv[1]) == -1) 
+                return err("error: cd: cannot change directory to "), err(argv[1]), err("\n"), 1;
+        return 0;
 }
 
 void set_pipe(int has_pipe, int *fd, int end)
 {
-	if(has_pipe && (dup2(fd[end], end) == -1 || close(fd[0]) == -1 || close(fd[1]) == -1))
-	{
-		errors("error: fatal dup\n");
-		exit(1);
-	}
+        if (has_pipe && (dup2(fd[end], end) == -1 || close(fd[0]) == -1 || close(fd[1]) == -1))
+                err("error: fatal\n"), exit(1);
 }
 
-
-int execute(char **argv, int i, char **env)
+int     exec(char **argv, int i, char **envp)
 {
-	int fd[2];
-	int pid;
-	int status;
-	int has_pipe;
-	has_pipe = argv[i] && !strcmp(argv[i], "|");
+        int has_pipe, fd[2], pid, status;
 
-	if (!has_pipe && !strcmp(*argv, "cd"))
-		return (exec_cd(*argv, i));
-	if (has_pipe && pipe(fd) == -1)
-	{
-		errors("error: fatal\n");
-		exit(1);
-	}
-	pid = fork();
-	if (pid == 0)
-	{
-		argv[i] = 0;
-		set_pipe(has_pipe, fd, 1);
-		if (!strcmp(*argv, "cd"))
-			return (exec_cd(*argv, i));
-		//printf("i in child is :%d\n",i);
-		//printf("execute argv: %s\n", argv[1]);
-		execve(*argv, argv, env);
-		errors("error: cannot execute "), errors(*argv), errors("\n");
-		exit(1);
-	}
-	waitpid(pid, &status, 0);
-    set_pipe(has_pipe, fd, 0);
-    return WIFEXITED(status) && WEXITSTATUS(status);
+        has_pipe = argv[i] && !strcmp(argv[i], "|");
+
+        if (!has_pipe && !strcmp(*argv, "cd"))
+                return cd(argv, i); 
+
+        if (has_pipe && pipe(fd) == -1) 
+                err("error: fatal\n"), exit(1);
+        pid = fork();
+        if (pid == 0)
+        {   
+                argv[i] = 0;
+                set_pipe(has_pipe, fd, 1); 
+                if (!strcmp(*argv, "cd"))
+                        return cd(argv, i); 
+                execve(*argv, argv, envp);
+                err("error: cannot execute "), err(*argv), err("\n"), exit(1);
+        }   
+        waitpid(pid, &status, 0); 
+        set_pipe(has_pipe, fd, 0); 
+
+        return(WIFEXITED(status) && WEXITSTATUS(status));
 }
 
-int main(int argc, char **argv, char **env)
+int main (int argc, char **argv, char **envp)
 {
-	int	i = 1;
-	int status = 0;
-	(void)argc;
+        (void)argc;
+        int i = 1;
+        int status = 0;
 
-	while(argv[i])
-	{
-		argv += i;
-		i = 0;
-		//printf("argv[%d]: %s\n", i, argv[i]);
-		while (argv[i] && strcmp(argv[i], "|") && strcmp(argv[i], ";"))
-			i++;
-		//printf("i is :%d\n",i);
-		if(i)
-			status = execute(argv, i, env);
-		//printf("Continues w next command: %s i: %d\n", argv[i], i);
-		if(argv[i] == NULL)
-			break;
-		i++;
-	}
-	return (status);
+        while(argv[i])
+        {   
+                argv = &argv[i];
+                i = 0;
+                while(argv[i] && strcmp(argv[i], "|") && strcmp(argv[i], ";"))
+                        i++;
+                if (i) 
+                        status = exec(argv, i, envp);
+                if (argv[i])
+                        i++;
+     }   
+     return status;
 }
